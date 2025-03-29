cb99d5c2186a4e018a39c3dfd14fabca18bf4838
zagorky <zagorkiynahyi@gmail.com>
2025-03-29 20:41:13 +0300

    feat: try to use popik

--- async-race/src/app.tsx
+++ async-race/src/app.tsx
@@ -1,16 +1,9 @@
 import { Popik } from '~/engine/popik.ts';
-import { Garage } from '~/view/garage/garage.tsx';
 
 export const App = (): JSX.Element => {
-  const [counter, setCounter] = Popik.useState(0);
-
-  return (
-    <Popik.Fragment>
-      <div>KELWIN, Spasssiba: {counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
-        I'm really button, click to "Counter" {counter}
-      </button>
-      <Garage />
-    </Popik.Fragment>
-  );
+  // const [counter, setCounter] = Popik.useState(0);
+  // const garageModel = createGarageModel();
+  // const cars = garageModel.getCars();
+  //
+  return <Popik.Fragment>{/*<CarContainer />*/}</Popik.Fragment>;
 };
